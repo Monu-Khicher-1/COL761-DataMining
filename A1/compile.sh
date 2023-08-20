312c02f083143e651c8aab15dc590962a254baa7
coolR <rcool7640@gmail.com>
2023-08-20 23:09:51 +0530

    code for A2

---
+++ A1/compile.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+# compile main.cpp file
+g++ -std=c++11 -o main main.cpp
---
+++ A1/interface.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+# run main executable with 3 inputs given though command line arguments
+./main $1 $2 $3
